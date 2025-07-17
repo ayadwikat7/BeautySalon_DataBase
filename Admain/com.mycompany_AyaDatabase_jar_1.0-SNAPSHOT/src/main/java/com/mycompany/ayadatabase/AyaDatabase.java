a4a0b9e4a27a8e73df8d3b67d7cc9b829a057a4c
ayadwikat7 <ayadwikat772@gmail.com>
2025-07-17 20:30:04 +0300

    Golden Beauty Salon

---
+++ Admain/com.mycompany_AyaDatabase_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/ayadatabase/AyaDatabase.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ayadatabase;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class AyaDatabase {
+
+    public static void main(String[] args) {
+        
+        BeautySaoln user=new BeautySaoln();
+        user.setVisible(true);
+         
+        
+    }
+}
